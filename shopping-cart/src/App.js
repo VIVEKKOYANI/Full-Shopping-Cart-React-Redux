856d8165dcb2bb8a792c2586f49ab39ddda51516
vivek <vivek.koyani@gmail.com>
2020-08-02 00:39:53 +0530

    add header and footer

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="grid-container">
+      <header>
+        <a href='/'>React Shopping Cart</a>
+      </header>
+      <main>Product List</main>
+      <footer>All right is reserved.</footer>
     </div>
   );
 }
